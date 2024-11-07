b481ee4315b4bc70a53171fad7e0208e287c0b5d
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-11-07 23:36:59 +0300

    fixed animation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,41 +1,36 @@
-const loader = document.querySelector('.loader');
-const content = document.querySelector('.main');
-const marquee = document.querySelector('.list-unstyled');
-const cursor = document.querySelector('.cursor');
-
-
-// 9. Лоадер
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", () => {
+    const loader = document.querySelector('.loader');
+    const content = document.querySelector('.main');
+    const marquee = document.querySelector('.list-unstyled');
+    const cursor = document.querySelector('.cursor');
+    // 9. Лоадер
     setTimeout(() => {
         loader.style.display = 'none';
         content.style.display = 'block';
+    // 6. Прокрутка галереи логотипов
+        const animation = anime({
+            targets: marquee,
+            translateX: [0, -marquee.scrollWidth / 2],
+            duration: 20000,
+            easing: 'linear',
+            loop: true,
+        });
+        const elements = document.querySelectorAll('.marquee ul li');
+        elements.forEach(el => {
+            el.addEventListener('mouseover', () => animation.pause());
+            el.addEventListener('mouseout', () => animation.play());
+        });
     }, 2000);
-};
-
-
-// 6. Прокрутка галереи логотипов
-const animation = anime({
-    targets: marquee,
-    translateX: [0, -marquee.scrollWidth / 2],
-    duration: 20000,
-    easing: 'linear',
-    loop: true,
-});
-const elements = document.querySelectorAll('.marquee ul li');
-elements.forEach(el => {
-    el.addEventListener('mouseover', () => animation.pause());
-    el.addEventListener('mouseout', () => animation.play());
-});
-
-// 8. Курсор
-document.addEventListener('mousemove', (e) => {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-    anime({
-        targets: cursor,
-        translateX: mouseX,
-        translateY: mouseY,
-        easing: 'easeOutQuad',
-        duration: 50,
+    // 8. Курсор
+    document.addEventListener('mousemove', (e) => {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        anime({
+            targets: cursor,
+            translateX: mouseX,
+            translateY: mouseY,
+            easing: 'easeOutQuad',
+            duration: 50,
+        });
     });
 });
